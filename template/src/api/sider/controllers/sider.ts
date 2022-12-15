1372cdd8a2cf729a006b842485c777a4f8d7afec
jjeem <alkheder.jad@gmail.com>
2022-12-15 19:22:59 +0300

    first commit

---
+++ template/src/api/sider/controllers/sider.ts
@@ -0,0 +1,7 @@
+/**
+ * sider controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sider.sider');
---
+++ template/src/api/sider/services/sider.ts
@@ -0,0 +1,7 @@
+/**
+ * sider service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sider.sider');
